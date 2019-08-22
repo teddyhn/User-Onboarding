83c3a28edc16cd8c8a711d4c0f29845bd9e8f447
teddyhn <tngo97@gmail.com>
2019-08-22 10:07:10 -0700

    Implemented axios and POST request

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
@@ -48,7 +49,10 @@ const FormikLoginForm = withFormik({
     }),
 
     handleSubmit(values) {
-        console.log(values);
+        axios
+            .post("https://reqres.in/api/users", values)
+            .then(res => console.log(res))
+            .catch(err => console.log(err.response));
     }
 })(LoginForm);
 
